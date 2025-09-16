e731843c1ddf2232a38463949640303e632d9b78
Sam <openminds.dev.studio@gmail.com>
2025-09-17 01:02:40 +0300

    chore: ignore build files

--- npm-scoped/src/src/log.ts
+++
@@ -1,8 +0,0 @@
-// Lightweight logger color exports for consistent CLI output.
-import {
-  bold,
-  gray,
-  green,
-  yellow,
-} from "../deps/jsr.io/@std/fmt/1.0.8/colors.js";
-export { bold, gray, green, yellow };
